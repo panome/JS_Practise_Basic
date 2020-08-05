61d80610d8a4fb841d71c8c515490cd4c12958a3
Ilya <Omeljan@post.cz>
2020-08-05 16:13:41 +0200

    Complete second homework

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt('Kolik filmu jste videl?')
+const numberOfFilms = +prompt('Kolik filmu jste videl?')
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,11 +8,22 @@ const personalMovieDB = {
     privat: false
 }
 
-const filmName1 = prompt('Jaky posledni film jste videl?'),
-      filmRating1 = prompt('Jak tento film ohodnotite?'),
-      filmName2 = prompt('Jaky posledni film jste videl?'),
-      filmRating2 = prompt('Jak tento film ohodnotite?')
+for (let i = 1; i < 3; i++) {
+	const filmName = prompt(`${i} Jaky posledni film jste videl?`),
+			filmRating = prompt(`${i} Jak tento film ohodnotite?`)
 
+	if (filmName != null && filmName != '' && filmRating != null && filmRating != '' && filmName.length < 50) {
+		personalMovieDB.movies[filmName] = filmRating
+	} else {
+		i--
+	}
+	
+}
 
-personalMovieDB.movies[filmName1] = filmRating1
-personalMovieDB.movies[filmName2] = filmRating2
+if (personalMovieDB.count < 10) {
+	alert('Videl jsi mene nez 10 filmu')
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+	alert('videl jsi vic nez 10 filmu ale mene nez 30')
+} else if (personalMovieDB.count > 30) {
+	alert('videl jsi vice nez 30 filmu')
+}
