bf081c7acc9794975ec6dee691b6d8842db5f21b
Ilya <Omeljan@post.cz>
2020-08-05 16:47:43 +0200

    Complete third homework

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const numberOfFilms = +prompt('Kolik filmu jste videl?')
+let numberOfFilms
+
+function start() {
+	numberOfFilms = +prompt('Kolik filmu jste videl?', '')
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Kolik filmu jste videl?', '')
+	}
+}
+start()
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,22 +17,43 @@ const personalMovieDB = {
     privat: false
 }
 
-for (let i = 1; i < 3; i++) {
-	const filmName = prompt(`${i} Jaky posledni film jste videl?`),
-			filmRating = prompt(`${i} Jak tento film ohodnotite?`)
+function rememberMyFilm() {
+	for (let i = 1; i < 3; i++) {
+		const filmName = prompt(`${i} Jaky posledni film jste videl?`, ''),
+				filmRating = prompt(`${i} Jak tento film ohodnotite?`, '')
 
-	if (filmName != null && filmName != '' && filmRating != null && filmRating != '' && filmName.length < 50) {
-		personalMovieDB.movies[filmName] = filmRating
-	} else {
-		i--
+		if (filmName != null && filmName != '' && filmRating != null && filmRating != '' && filmName.length < 50) {
+			personalMovieDB.movies[filmName] = filmRating
+		} else {
+			i--
+		}
+		
 	}
-	
 }
+rememberMyFilm()
 
-if (personalMovieDB.count < 10) {
-	alert('Videl jsi mene nez 10 filmu')
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-	alert('videl jsi vic nez 10 filmu ale mene nez 30')
-} else if (personalMovieDB.count > 30) {
-	alert('videl jsi vice nez 30 filmu')
+function detectPersonalLevel() {
+	if (personalMovieDB.count <= 10) {
+		alert('Videl jsi mene nez 10 filmu')
+	} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+		alert('videl jsi vic nez 10 filmu ale mene nez 30')
+	} else if (personalMovieDB.count > 30) {
+		alert('videl jsi vice nez 30 filmu')
+	}
 }
+detectPersonalLevel()
+
+function writeYourGenres() {
+	for (let i = 0; i < 3; i++) {
+		const genreAnswer = prompt(`Jaky je vas oblibeny zanr cislo ${i + 1}`)
+		personalMovieDB.genres[i] = genreAnswer
+	}
+}
+writeYourGenres()
+
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB)
+	}
+}
+showMyDB(personalMovieDB.privat)
